cb1246bbbfaa5ae5fc47b6d8fc761ee19ee853d3
ahmadazimi <ferdous.azimi7@gmail.com>
2024-06-23 16:45:52 +1000

    Animated Cursor added.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import AnimatedCursor from "react-animated-cursor";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <AnimatedCursor color="255,255,255" />
   </React.StrictMode>
 );
